c31b411404d12f0a1bb66b02fee3d0b05fb777ec
Samir Omer <samiromer2@yahoo.com>
2024-09-11 11:19:42 -0700

    code base ready , blank page made

---
+++ admin/html/meno.php
@@ -0,0 +1,150 @@
+        <!-- Menu -->
+  
+        <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+          <div class="app-brand demo">
+            <a href="index.html" class="app-brand-link">
+              <span class="app-brand-logo demo me-1">
+                <span style="color: var(--bs-primary)">
+                  
+                </span>
+              </span>
+              <span class="app-brand-text demo menu-text fw-semibold ms-2">AlQots</span>
+            </a>
+
+            <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto">
+             
+            </a>
+          </div>
+
+          <div class="menu-inner-shadow"></div>
+
+          <ul class="menu-inner py-1">
+            <!-- Dashboards -->
+            <li class="menu-item">
+              <a href="javascript:void(0);" class="menu-link menu-toggle">
+                <i class="menu-icon tf-icons ri-home-smile-line"></i>
+                <div data-i18n="Dashboards">Dashboards</div>
+              
+              </a>
+              <ul class="menu-sub">
+                <li class="menu-item">
+                  <a
+                    href="https://demos.themeselection.com/materio-bootstrap-html-admin-template/html/vertical-menu-template/dashboards-crm.html"
+                    target="_blank"
+                    class="menu-link">
+                    <div data-i18n="CRM">HomePage</div>
+                  </a>
+                </li>
+              </ul>
+            </li>
+
+            <li class="menu-header mt-7">
+              <span class="menu-header-text">Apps &amp; Pages</span>
+            </li>
+
+            <!-- Pages -->
+            <li class="menu-item">
+              <a href="javascript:void(0);" class="menu-link menu-toggle">
+                <i class="menu-icon tf-icons ri-layout-left-line"></i>
+                <div data-i18n="Account Settings">Account Settings</div>
+              </a>
+              <ul class="menu-sub">
+                <li class="menu-item">
+                  <a href="pages-account-settings-account.html" class="menu-link">
+                    <div data-i18n="Account">Account</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="pages-account-settings-notifications.html" class="menu-link">
+                    <div data-i18n="Notifications">Notifications</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="pages-account-settings-connections.html" class="menu-link">
+                    <div data-i18n="Connections">Connections</div>
+                  </a>
+                </li>
+              </ul>
+            </li>
+			
+ 
+            <li class="menu-item active open">
+              <a href="javascript:void(0);" class="menu-link menu-toggle">
+                <i class="menu-icon tf-icons ri-box-3-line"></i>
+                <div data-i18n="Form Layouts">Form Layouts</div>
+              </a>
+              <ul class="menu-sub">
+                <li class="menu-item">
+                  <a href="form-layouts-vertical.html" class="menu-link">
+                    <div data-i18n="Vertical Form">Vertical Form</div>
+                  </a>
+                </li>
+                <li class="menu-item active">
+                  <a href="form-layouts-horizontal.html" class="menu-link">
+                    <div data-i18n="Horizontal Form">Horizontal Form</div>
+                  </a>
+                </li>
+              </ul>
+            </li>
+		            <li class="menu-header mt-7">
+              <span class="menu-header-text">Reports</span>
+            </li>
+         
+		             <li class="menu-item">
+              <a
+                href="#"
+                target="_blank"
+                class="menu-link">
+                <i class="menu-icon tf-icons ri-wechat-line"></i>
+                <div data-i18n="report1">report1</div>
+                <div class="badge bg-label-primary fs-tiny rounded-pill ms-auto">cash</div>
+              </a>
+            </li>
+		 
+
+          </ul>
+        </aside>
+        <!-- / Menu -->
+		
+		
+		        <!-- Layout container -->
+        <div class="layout-page">
+          <!-- Navbar -->
+
+          <nav
+            class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
+            id="layout-navbar">
+            <div class="layout-menu-toggle navbar-nav align-items-xl-center me-4 me-xl-0 d-xl-none">
+              <a class="nav-item nav-link px-0 me-xl-6" href="javascript:void(0)">
+                <i class="ri-menu-fill ri-24px"></i>
+              </a>
+            </div>
+
+            <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
+              <!-- Search -->
+             
+              <!-- /Search -->
+
+              <ul class="navbar-nav flex-row align-items-center ms-auto">
+                <!-- Place this tag where you want the button to render. -->
+                <li class="nav-item lh-1 me-4">
+                  <a href="#"><button type="button" class="btn rounded-pill btn-dark">LogOut</button></a>
+				   
+                </li>
+
+              </ul>
+            </div>
+          </nav>
+
+
+          <div class="content-wrapper">
+            <!-- Content -->
+
+            <div class="container-xxl flex-grow-1 container-p-y">
+              <!-- Basic Layout & Basic with Icons -->
+              <div class="row">
+                <!-- Basic Layout -->
+                <div class="col-xxl">
+                  <div class="card mb-6">
+                    <div class="card-header d-flex align-items-center justify-content-between">
+                     
---
+++ admin/html/page1.php
@@ -0,0 +1,11 @@
+<?php
+include ("frontcode.php");
+include ("meno.php");
+?>
+
+<h5 class="mb-0">Basic Layout</h5></div>
+<div class="card-body">
+blank page
+                  
+
+<?php include("rear.php");?>
